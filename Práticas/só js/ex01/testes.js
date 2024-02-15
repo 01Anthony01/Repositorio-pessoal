f679e0ac372328be2530abf079ebb6bba9004662
Anthony <Anthonymiranda871@gmail.com>
2024-02-15 14:00:22 -0300

    adicionando um projeto já feito antes e alguns testes

--- Práticas/só js/ex01/testes.js
+++ Práticas/só js/ex01/testes.js
@@ -1,10 +1,16 @@
 let informacoes = ['Anthony', 17 , 'brasil']
 console.log(`OI meu nome é ${informacoes[0]} eu tenho ${informacoes[1]} anos e eu moro no ${informacoes[2]}`)
+
+
 if(informacoes[1] <= 17){
     console.log('voce é de menor')
 }else{
     console.log('voce é maior')
+
 }
+
+
+
 for(let c = 1 ;c <= 20 ; c = c + 1){
 
     console.log(c)
