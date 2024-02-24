1c2a8ae2e6dce584e41f358563746813d9aa0e87
Anthony <Anthonymiranda871@gmail.com>
2024-02-24 12:51:37 -0300

    algumas práticas

---
+++ Práticas/só js/exs/ex02/ex04.js
@@ -0,0 +1,18 @@
+let pessoa = {
+    nome: 'anthony' , idade: 17 , cidade: 'são paulo'
+}
+
+var chaves = Object.keys(pessoa)
+console.log(chaves)
+
+
+let carro = {
+    marca: 'chevrolet',
+    modelo: 'camaro',
+    ano: 2024,
+    cor: 'preto'
+}
+
+for(let chave in carro){
+    console.log(chave + ": " + pessoa[chave]);
+}
---
+++ arrays e objetos/01/arrayseobjetos.js
@@ -0,0 +1,19 @@
+var arrayempresas = [
+     {
+        rua: 'sao joao' ,
+        cep: 1002201 ,
+        casa: '' ,
+        autor: 'anthony'
+    },
+    {
+        rua: 'opa jair',
+        cep: 10102,
+        autor: 'miranda',
+        casa: ''
+
+    }
+
+
+]
+
+console.log(arrayempresas[0])
