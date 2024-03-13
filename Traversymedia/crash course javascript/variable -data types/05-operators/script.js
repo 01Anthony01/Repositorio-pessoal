7ae8524f2283949fd7165f9b8f5938165019c678
Anthony <Anthonymiranda871@gmail.com>
2024-03-13 20:40:33 -0300

    operadores

---
+++ Traversymedia/crash course javascript/variable -data types/05-operators/script.js
@@ -0,0 +1,31 @@
+// Operadores aritméticos
+
+let x = ''
+x = 8+5*2/2%9-3
+
+console.log(x)
+
+// expoente
+x = x**3
+console.log(x)
+
+// incremento
+x = 1
+x = x + 1
+// ou
+x++
+console.log(x)
+
+// decremento
+x = 10
+x = x - 1
+// ou
+x--
+
+console.log(x)
+
+// operadores de atribuição
+// basicamente eu posso usar todos os operadores aritmeticos junto com o sinal de iguap = e transformar eles em operadores de atribuição
+x = 50
+
+// operadores relacionais
