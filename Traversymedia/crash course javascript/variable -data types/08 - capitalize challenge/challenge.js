c36c2a267a01e1b598cc056e5369a020dd9826ca
Anthony <Anthonymiranda871@gmail.com>
2024-03-19 15:39:37 -0300

    praticando e estudando sobre métodos e nos valores primitivos number e realizando um desafio

---
+++ Traversymedia/crash course javascript/variable -data types/08 - capitalize challenge/challenge.js
@@ -0,0 +1,9 @@
+// esse é o desafio do traversy eu tenho que pegar o valor da variavel e transformar só a primeira letra desse valor em maiusculo
+
+let mystring = 'developer'
+
+
+console.log(mystring.charAt(0).toUpperCase() + mystring.substring(1 , 9))
+
+//soluçao, acima foi a minha melhor tentativa:
+//  PUTA QUE PARIU A 1 SOLUCAO DELE FOI IGUAL A MINHA SO FALTOU EU PENSAR EM CONCATENAR A STRING QUE RAIVA SO FALTOU UM SINALZINHO DE +
---
+++ Traversymedia/crash course javascript/variable -data types/09 - numbers/codigo.js
@@ -0,0 +1,22 @@
+let x;
+
+const num = new Number(5.670) 
+
+//métodos que podemos usar com o tipo primitivo number
+
+x = num.toString()
+
+x = num.toString().length
+
+x = num.toFixed(3)
+
+x = num.toPrecision(2)
+
+x = num.toExponential(3)
+
+x = num.toLocaleString('en-US') // isso aqui serve para saber como é a representacao dos numeros ou letras em outros idiomas
+
+x = Number.MIN_VALUE
+X = Number.MAX_VALUE
+
+console.log(x);
